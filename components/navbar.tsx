f75d70b485ac256334af1959c242a54a864369cf
Mika Munterud <mika.munterud@outlook.com>
2023-10-17 01:17:39 +0200

    Adding category fetch to navbar and dropdown menu view

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,33 +1,58 @@
 'use client';
 
+import getCategories from '@/actions/get-categories';
 import {
   NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
   NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
 } from '@/components/ui/navigation-menu';
+import { Category } from '@/types';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-interface NavbarProps {
-  links: { label: string; href: string; id: string }[];
-  currentUrl: string;
-}
+export default function Navbar() {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const pathname = usePathname();
 
-export default function Navbar({ links, currentUrl }: NavbarProps) {
+  useEffect(() => {
+    async function getData() {
+      const categories = await getCategories();
+
+      setCategories(categories);
+    }
+    getData();
+  }, []);
+
   return (
     <NavigationMenu className="mx-6 flex items-center space-x-4 lg:space-x-6">
-      {links.map(function (link, index: any) {
-        const isActive = `/category/${link.id}` === currentUrl;
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Category</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-4 lg:grid-cols-[.75fr_1fr]">
+              {categories.map(function (category) {
+                const isActive = `/category/${category.id}` === pathname;
 
-        return (
-          <NavigationMenuLink
-            className={`text-sm font-medium transition-colors hover:text-black ${
-              isActive ? 'text-black' : 'text-neutral-500'
-            }`}
-            key={index}
-            href={`/category${link.href}`}
-          >
-            {link.label}
-          </NavigationMenuLink>
-        );
-      })}
+                return (
+                  <li className="row-span-3" key={category.id}>
+                    <NavigationMenuLink
+                      className={`text-sm font-medium transition-colors hover:text-black   ${
+                        isActive ? 'text-black' : 'text-neutral-500'
+                      }`}
+                      href={`/category/${category.id}`}
+                    >
+                      {category.name}
+                    </NavigationMenuLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
     </NavigationMenu>
   );
 }
