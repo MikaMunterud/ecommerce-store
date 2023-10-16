966539b0e04568300f89ac84f960a50a6252bf14
Mika Munterud <mika.munterud@outlook.com>
2023-10-16 16:01:06 +0200

    Removing console.log

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   useEffect(() => {
     async function getData() {
       const products = await getProducts();
-      console.log(products);
+
       setProducts(products);
     }
     getData();
