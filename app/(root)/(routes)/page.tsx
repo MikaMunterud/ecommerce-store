01871e10f444fe22e449065ed399f77c164ca663
Mika Munterud <mika.munterud@outlook.com>
2023-10-16 12:08:41 +0200

    Adding base url from env file

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import HeroBanner from "@/components/hero-banner";
-import ProductCard from "@/components/product-card";
-import { useEffect, useState } from "react";
+import HeroBanner from '@/components/hero-banner';
+import ProductCard from '@/components/product-card';
+import { useEffect, useState } from 'react';
 
 type Products = {
   id: string;
@@ -10,21 +10,21 @@ type Products = {
   price: number;
   imageUrl: string;
   categoryId: string;
-  images: string;
+  img: string;
 };
 
 export default function Home() {
   const [products, setProducts] = useState<Products[]>([]);
 
   const heroBanner = {
-    label: "E-commerce Store",
-    url: "https://media.istockphoto.com/id/1316968335/sv/foto/kundvagn-full-av-mat-p%C3%A5-gul-bakgrund-mataff%C3%A4rskoncept.jpg?s=612x612&w=0&k=20&c=5NBtN9fhss48STDiV-KAdYkpmc6tB8LEcdKF2nEaT44=",
+    label: 'E-commerce Store',
+    url: 'https://media.istockphoto.com/id/1316968335/sv/foto/kundvagn-full-av-mat-p%C3%A5-gul-bakgrund-mataff%C3%A4rskoncept.jpg?s=612x612&w=0&k=20&c=5NBtN9fhss48STDiV-KAdYkpmc6tB8LEcdKF2nEaT44=',
   };
 
   useEffect(() => {
     async function getProducts() {
       const response = await fetch(
-        "https://ecommerce-dashboard-kohl.vercel.app/api/47844042-830e-489a-a010-ab5c442bb816/products"
+        `${process.env.NEXT_PUBLIC_API_URL}/products`,
       );
       const products = await response.json();
       setProducts(products);
@@ -46,7 +46,7 @@ export default function Home() {
               id={product.id}
               label={product.name}
               price={product.price}
-              imageUrl={product.images}
+              imageUrl={product.img}
             />
           ))}
         </div>
