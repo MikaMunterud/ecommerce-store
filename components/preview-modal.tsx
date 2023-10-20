29a64abb132452495aaea1a732e85b192b7eb3eb
Mika Munterud <mika.munterud@outlook.com>
2023-10-20 19:29:46 +0200

    Replacing Gallery component with just one image

--- components/preview-modal.tsx
+++ components/preview-modal.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import usePreviewModal from '@/hooks/use-preview-modal';
-import Gallery from '@/components/gallery';
 import Info from '@/components/info';
 import Modal from '@/components/ui/modal';
+import Image from 'next/image';
 
 const PreviewModal = () => {
   const previewModal = usePreviewModal();
@@ -17,7 +17,14 @@ const PreviewModal = () => {
     <Modal open={previewModal.isOpen} onClose={previewModal.onClose}>
       <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
         <div className="sm:col-span-4 lg:col-span-5">
-          <Gallery image={product.img} />
+          <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
+            <Image
+              fill
+              src={product.img}
+              alt="Image"
+              className="object-cover object-center"
+            />
+          </div>
         </div>
         <div className="sm:col-span-8 lg:col-span-7">
           <Info data={product} />
