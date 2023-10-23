9d7221a0132d364d21d17d9ba78b96b9d1c5d13f
Mika Munterud <mika.munterud@outlook.com>
2023-10-23 09:54:37 +0200

    Updating getProduct action

--- actions/get-product.tsx
+++ actions/get-product.tsx
@@ -1,67 +1,41 @@
-import { Category, Color, FormattedProduct, Product, Size } from '@/types';
+import { FormattedProduct } from '@/types';
 import axios from 'axios';
 import { format } from 'date-fns';
 
-export async function getProduct(id: string) {
+import { getCategory } from './get-category';
+import { getSize } from './get-size';
+import { getColor } from './get-color';
+
+export async function getProduct(id: string): Promise<FormattedProduct | null> {
   const response = await axios.get(
     `${process.env.NEXT_PUBLIC_API_URL}/products/${id}`,
   );
   const data = await response.data;
 
   if (data) {
-    const categories = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/categories`,
-    );
-    const categoriesData = await categories.data;
+    const category = await getCategory(data.categoryId);
 
-    const sizes = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/sizes`);
-    const sizesData = await sizes.data;
+    const size = await getSize(data.sizeId);
 
-    const colors = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/colors`);
-    const colorsData = await colors.data;
+    const color = await getColor(data.colorId);
 
-    const formattedProducts = await formatProduct(
-      data,
-      categoriesData,
-      sizesData,
-      colorsData,
-    );
+    const formattedProducts = {
+      id: data.id,
+      name: data.name,
+      description: data.description,
+      price: Number(data.price).toFixed(2),
+      categoryId: data.categoryId,
+      category: category.name,
+      size: size.name,
+      color: color.name,
+      colorValue: color.value,
+      isFeatured: data.isFeatured,
+      created: format(new Date(data.created), 'do MMM yyyy'),
+      img: data.img,
+    };
 
     return formattedProducts;
   } else {
     return null;
   }
-}
-
-async function formatProduct(
-  product: Product,
-  categories: Category[],
-  sizes: Size[],
-  colors: Color[],
-) {
-  const formattedProducts = {
-    id: product.id,
-    name: product.name,
-    description: product.description,
-    price: Number(product.price).toFixed(2),
-    categoryId: product.categoryId,
-    category: categories.find(function (category: Category) {
-      return category.id === product.categoryId;
-    })?.name,
-    size: sizes.find(function (size: Size) {
-      return size.id === product.sizeId;
-    })?.name,
-
-    color: colors.find(function (color: Color) {
-      return color.id === product.colorId;
-    })?.name,
-    colorValue: colors.find(function (color: Color) {
-      return color.id === product.colorId;
-    })?.value,
-    isFeatured: product.isFeatured,
-    created: format(new Date(product.created), 'do MMM yyyy'),
-    img: product.img,
-  };
-
-  return formattedProducts;
 }
