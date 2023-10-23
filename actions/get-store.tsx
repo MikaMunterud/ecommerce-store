86a609f593c820e8b777e578c5711e64d7571fee
Mika Munterud <mika.munterud@outlook.com>
2023-10-23 09:55:30 +0200

    Adding promise to getStore action

--- actions/get-store.tsx
+++ actions/get-store.tsx
@@ -1,8 +1,9 @@
+import { Store } from '@/types';
 import axios from 'axios';
 
 const url = process.env.NEXT_PUBLIC_API_URL;
 
-export async function getStore() {
+export async function getStore(): Promise<Store> {
   const modifiedURL = url?.replace('/api', '/api/stores');
 
   const response = await axios.get(`${modifiedURL}`);
