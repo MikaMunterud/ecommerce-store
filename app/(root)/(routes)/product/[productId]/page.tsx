cf12b99c7d5fa8fa73911f65b3bd7311a6a5b11e
Mika Munterud <mika.munterud@outlook.com>
2023-10-23 09:57:26 +0200

    Updating product page

--- app/(root)/(routes)/product/[productId]/page.tsx
+++ app/(root)/(routes)/product/[productId]/page.tsx
@@ -14,7 +14,7 @@ import toast from 'react-hot-toast';
 
 export default function ProductPage() {
   const { productId } = useParams<{ productId: string }>();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const [product, setProduct] = useState<FormattedProduct | any>();
   const [products, setProducts] = useState<FormattedProduct[] | any>([]);
@@ -24,7 +24,6 @@ export default function ProductPage() {
       getData();
 
       async function getData() {
-        setLoading(true);
         const product = await getProduct(productId);
 
         setProduct(product);
@@ -46,14 +45,6 @@ export default function ProductPage() {
     );
   }
 
-  if (!product) {
-    return (
-      <div className="px-4 py-10 sm:px-6 lg:px-8">
-        <h1 className="font-bold text-3xl">Product not found...</h1>
-      </div>
-    );
-  }
-
   const relatedProducts = products.filter(function (p: FormattedProduct) {
     return (
       p.categoryId === product.categoryId &&
@@ -64,36 +55,43 @@ export default function ProductPage() {
 
   return (
     <div>
-      <div className="px-4 py-10 sm:px-6 lg:px-8">
-        <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-          <Image
-            src={product.img}
-            alt={product.name}
-            height={400}
-            width={400}
-            className="object-cover object-center p-6"
-          />
-          <div>
-            <Info data={product} />
+      {product ? (
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            <Image
+              priority
+              src={product.img}
+              alt={product.name}
+              height={400}
+              width={400}
+              className="object-cover object-center p-6"
+            />
+            <div>
+              <Info data={product} />
+            </div>
           </div>
+
+          <Separator className="my-10" />
+
+          <h2 className="font-bold text-3xl mb-8">{'Related products'}</h2>
+
+          {relatedProducts.length > 0 ? (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {relatedProducts.map((product: FormattedProduct) => (
+                <ProductCard key={product.id} data={product} />
+              ))}
+            </div>
+          ) : (
+            <p className="text-gray-500 font-bold text-xl px-2">
+              No related products found...
+            </p>
+          )}
         </div>
-
-        <Separator className="my-10" />
-
-        <h2 className="font-bold text-3xl mb-8">{'Related products'}</h2>
-
-        {relatedProducts.length > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {relatedProducts.map((product: FormattedProduct) => (
-              <ProductCard key={product.id} data={product} />
-            ))}
-          </div>
-        ) : (
-          <p className="text-gray-500 font-bold text-xl px-2">
-            No related products found...
-          </p>
-        )}
-      </div>
+      ) : (
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <h1 className="font-bold text-3xl">Product not found...</h1>
+        </div>
+      )}
     </div>
   );
 }
